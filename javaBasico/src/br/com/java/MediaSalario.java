375d0ec302d2f8d4c1a7d4d663d3413aebe592e4
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-16 16:57:00 -0300

    criação do arquivo classe principal para media salarial.

---
+++ javaBasico/src/br/com/java/MediaSalario.java
@@ -0,0 +1,21 @@
+package br.com.java;
+
+public class MediaSalario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double n1, n2, n3, n4;
+		double media;
+		
+		n1 = 3453.21;
+		n2 = 3498.43;
+		n3 = 7902.09;
+		n4 = 12932.00;
+		
+		media = (n1 + n2 + n3 + n4 ) / 4;
+		
+		System.out.println("A média salarial da empresa é de R$ " + media);
+	}
+
+}
